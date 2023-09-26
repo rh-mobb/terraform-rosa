93d1f0a731d6f677c1b75ea21c9e4c01d13be496
Dustin Scott <dustin.scott18@gmail.com>
2023-09-26 11:32:57 -0500

    fix: corrected security group port range for ingress
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- bastion.tf
+++ bastion.tf
@@ -49,6 +49,7 @@ resource "aws_security_group" "bastion_host" {
     description = "Bastion SSH Ingress"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
