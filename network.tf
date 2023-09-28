8efd0157a005bd25cab4bf5a3b8a65e3705858dc
Dustin Scott <dustin.scott18@gmail.com>
2023-09-28 12:57:38 -0500

    feat: add subnet_cidr_size input
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- network.tf
+++ network.tf
@@ -6,7 +6,7 @@ module "network" {
     multi_az           = var.multi_az
     vpc_network        = split("/", var.vpc_cidr)[0]
     vpc_cidr_size      = tonumber(split("/", var.vpc_cidr)[1])
-    subnet_cidr_size   = 20
+    subnet_cidr_size   = var.subnet_cidr_size
     public_subnet_ids  = []
     private_subnet_ids = []
   }
