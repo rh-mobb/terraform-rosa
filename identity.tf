6118050e08badc66050240f680c32ac2110982cc
Dustin Scott <dustin.scott18@gmail.com>
2024-01-23 13:47:31 -0600

    feat: add tags and identity provider
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

---
+++ identity.tf
@@ -0,0 +1,27 @@
+resource "rhcs_identity_provider" "admin" {
+  count = var.admin_password != null && var.admin_password != "" ? 1 : 0
+
+  cluster = rhcs_cluster_rosa_classic.rosa.id
+  name    = "admin"
+  htpasswd = {
+    users = [{
+      username = "admin"
+      password = "<user-password>"
+      },
+    ]
+  }
+}
+
+resource "rhcs_identity_provider" "developer" {
+  count = var.developer_password != null && var.developer_password != "" ? 1 : 0
+
+  cluster = rhcs_cluster_rosa_classic.rosa.id
+  name    = "developer"
+  htpasswd = {
+    users = [{
+      username = "developer"
+      password = "<user-password>"
+      },
+    ]
+  }
+}
