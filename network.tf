d5fc15db2993e5737f2dbe42a2bebb04bd14e71b
Dustin Scott <dustin.scott18@gmail.com>
2024-01-23 14:56:29 -0600

    feat: use 0.0.2 of network
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/scottd018-demos/terraform-rosa-networking.git?ref=v0.0.1"
+  source = "git::https://github.com/scottd018-demos/terraform-rosa-networking.git?ref=v0.0.2"
 
   network = {
     private_link       = var.private
