fadbe8c2bbdab38ddc0de52016329a6bb927511f
Dustin Scott <dustin.scott18@gmail.com>
2024-02-19 15:29:32 -0600

    feat: add outputs
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.network.private_subnet_ids
+}
