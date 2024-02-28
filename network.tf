18cf01c9d480c43f630e7e9ed577867c568d63d1
Dustin Scott <dustin.scott18@gmail.com>
2024-02-28 14:51:10 -0600

    feat: fix private subnets
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/scottd018-demos/terraform-rosa-networking.git?ref=v0.0.4"
+  source = "git::https://github.com/scottd018-demos/terraform-rosa-networking.git?ref=v0.0.5"
 
   network = {
     cluster_name       = var.cluster_name
