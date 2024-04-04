ac3e9dac877f7cb3e3cacdccebc29eb59fbef12f
Dustin Scott <dustin.scott18@gmail.com>
2024-04-04 16:26:16 -0500

    feat: add oidc attrs as output
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,11 @@ output "public_subnet_azs" {
 output "private_subnet_azs" {
   value = module.network.private_subnet_azs
 }
+
+output "oidc_config_id" {
+  value = rhcs_cluster_rosa_classic.rosa.sts.oidc_config_id
+}
+
+output "oidc_endpoint_url" {
+  value = rhcs_cluster_rosa_classic.rosa.sts.oidc_endpoint_url
+}
